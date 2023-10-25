92d7c80babbaf132f5dd8b5a7ddeb3370a383d0f
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2023-10-25 09:03:53 -0400

    force rebuild

--- components/Footer.js
+++ components/Footer.js
@@ -5,41 +5,45 @@ import HoverIcon from "./HoverIcon";
 import { showNotification } from "@mantine/notifications";
 
 export default function Footer() {
-  return (
-    <>
-      <footer className="m-auto max-w-[240px] flex justify-around py-5">
-        <HoverIcon
-          name="Github"
-          iconName="akar-icons:github-fill"
-          href="https://github.com/a-knaw-knee-mus"
-        />
-        <HoverIcon
-          name="Linkedin"
-          iconName="akar-icons:linkedin-box-fill"
-          href="https://www.linkedin.com/in/muhammad-mehdi-ali/"
-        />
-        <HoverIcon
-          name="Email"
-          iconName="ic:baseline-email"
-          href="mailto:m30ali@torontmu.ca"
-        />
-        <a
-          className="group font-overpass text-xs sm:text-sm text-center cursor-pointer"
-          onClick={() => {
-            navigator.clipboard.writeText("a.knaw.knee.mus");
-            showNotification({
-              color: "indigo",
-              title: "Copied to clipboard!",
-              message: "a.knaw.knee.mus"
-            });
-          }}
-        >
-          <Icon className="mx-auto" icon="akar-icons:discord-fill" width="40" />
-          <p className="sm:opacity-0 sm:group-hover:opacity-100 transition-all">
-            Discord
-          </p>
-        </a>
-      </footer>
-    </>
-  );
+    return (
+        <>
+            <footer className="m-auto max-w-[240px] flex justify-around py-5">
+                <HoverIcon
+                    name="Github"
+                    iconName="akar-icons:github-fill"
+                    href="https://github.com/a-knaw-knee-mus"
+                />
+                <HoverIcon
+                    name="Linkedin"
+                    iconName="akar-icons:linkedin-box-fill"
+                    href="https://www.linkedin.com/in/muhammad-mehdi-ali/"
+                />
+                <HoverIcon
+                    name="Email"
+                    iconName="ic:baseline-email"
+                    href="mailto:m30ali@torontmu.ca"
+                />
+                <a
+                    className="group font-overpass text-xs sm:text-sm text-center cursor-pointer"
+                    onClick={() => {
+                        navigator.clipboard.writeText("a.knaw.knee.mus");
+                        showNotification({
+                            color: "indigo",
+                            title: "Copied to clipboard!",
+                            message: "a.knaw.knee.mus",
+                        });
+                    }}
+                >
+                    <Icon
+                        className="mx-auto"
+                        icon="akar-icons:discord-fill"
+                        width="40"
+                    />
+                    <p className="sm:opacity-0 sm:group-hover:opacity-100 transition-all">
+                        Discord
+                    </p>
+                </a>
+            </footer>
+        </>
+    );
 }
