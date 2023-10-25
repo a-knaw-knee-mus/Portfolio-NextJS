c2163d44a963ae49d3a6dd3360145825c0d0d7af
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2023-10-25 09:17:31 -0400

    add isr

--- util/getHomeContent.js
+++ util/getHomeContent.js
@@ -10,6 +10,6 @@ export default async function getHomeContent() {
     "resumeUrl": resume.asset->url,    
     }`);
   const url = `https://219pd81c.api.sanity.io/v1/data/query/production?query=${query}`;
-  const result = await fetch(url).then((res) => res.json());
+  const result = await fetch(url, { next: { revalidate: 10 } }).then((res) => res.json());
   return result.result[0];
 }
--- util/getProjects.js
+++ util/getProjects.js
@@ -14,6 +14,6 @@ export default async function getProjects(slug) {
     }`);
 
     const url = `https://219pd81c.api.sanity.io/v1/data/query/production?query=${query}`;
-    const result = await fetch(url).then((res) => res.json());
+    const result = await fetch(url, { next: { revalidate: 10 } }).then((res) => res.json());
     return result.result
 }
