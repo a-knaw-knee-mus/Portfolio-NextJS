452c21a53c3081711766b0963c3d16a472e589f8
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2024-05-11 11:27:12 -0400

    optionally show github and youtube links

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -22,19 +22,19 @@ const components = {
 const item = {
     hidden: { opacity: 0 },
     visible: {
-      y: 0,
-      opacity: 1
-    }
+        y: 0,
+        opacity: 1,
+    },
 };
 
 export default function ProjectCard({ post, projectEnter, projectLeave }) {
     const router = useRouter();
-    const [cursorText, setCursorText] = useCursorTextAtom()
-    const [cursorVariant, setCursorVariant] = useCursorVariantAtom()
+    const [cursorText, setCursorText] = useCursorTextAtom();
+    const [cursorVariant, setCursorVariant] = useCursorVariantAtom();
 
     return (
         <motion.div
-            whileHover={{scale: 1.05}}
+            whileHover={{ scale: 1.05 }}
             variants={item}
             className="mx-auto m-3 shadow-xl relative rounded-lg text-black dark:shadow-blue-900 dark:shadow-lg"
             key={post._id}
@@ -51,58 +51,68 @@ export default function ProjectCard({ post, projectEnter, projectLeave }) {
                     {post.title}
                 </motion.h1>
                 <motion.div layoutId={post._id}>
-                <Image
-                    className="rounded-t-lg cursor-pointer sm:cursor-none"
-                    onClick={() => router.push(`/projects/${post.slug}`)}
-                    onMouseEnter={() => {
-                        setCursorText("humbleicons:expand")
-                        setCursorVariant("project")
-                    }}
-                      onMouseLeave={() => {
-                        setCursorText("")
-                        setCursorVariant("default")
-                    }}
-                    src={getSanityImage(post.mainImage).width(576).height(324).url()}
-                    alt="project preview"
-                    height={324}
-                    width={576}
-                />
+                    <Image
+                        className="rounded-t-lg cursor-pointer sm:cursor-none"
+                        onClick={() => router.push(`/projects/${post.slug}`)}
+                        onMouseEnter={() => {
+                            setCursorText("humbleicons:expand");
+                            setCursorVariant("project");
+                        }}
+                        onMouseLeave={() => {
+                            setCursorText("");
+                            setCursorVariant("default");
+                        }}
+                        src={getSanityImage(post.mainImage)
+                            .width(576)
+                            .height(324)
+                            .url()}
+                        alt="project preview"
+                        height={324}
+                        width={576}
+                    />
                 </motion.div>
                 <div
                     className="transition-all flex absolute top-0 right-0 bg-slate-50 bg-opacity-50 
                                     rounded-bl-lg rounded-tr-lg md:group-hover:bg-opacity-100"
                 >
-                    <motion.a
-                        layoutId={`${post._id}-demo`}
-                        target="_blank"
-                        href={post.demo}
-                        rel="noopener noreferrer"
-                        className="p-3 z-40"
-                    >
-                        <Icon
-                            className="transition-all sm:hover:scale-[1.15]"
-                            icon="eva:external-link-fill"
-                            width="30"
-                        />
-                    </motion.a>
-                    <motion.a
-                        layoutId={`${post._id}-github`}
-                        target="_blank"
-                        href={post.github}
-                        rel="noopener noreferrer"
-                        className="p-3 z-40"
-                    >
-                        <Icon
-                            className="transition-all sm:hover:scale-[1.15]"
-                            icon="akar-icons:github-fill"
-                            width="30"
-                        />
-                    </motion.a>
+                    {post.demo && (
+                        <motion.a
+                            layoutId={`${post._id}-demo`}
+                            target="_blank"
+                            href={post.demo}
+                            rel="noopener noreferrer"
+                            className="p-3 z-40"
+                        >
+                            <Icon
+                                className="transition-all sm:hover:scale-[1.15]"
+                                icon="eva:external-link-fill"
+                                width="30"
+                            />
+                        </motion.a>
+                    )}
+                    {post.github && (
+                        <motion.a
+                            layoutId={`${post._id}-github`}
+                            target="_blank"
+                            href={post.github}
+                            rel="noopener noreferrer"
+                            className="p-3 z-40"
+                        >
+                            <Icon
+                                className="transition-all sm:hover:scale-[1.15]"
+                                icon="akar-icons:github-fill"
+                                width="30"
+                            />
+                        </motion.a>
+                    )}
                 </div>
             </div>
             <motion.div layoutId={`${post._id}-message`} className="max-w-xl">
                 <Accordion variant="filled">
-                    <Accordion.Item value={post.summary} className="mt-0 rounded-b-lg">
+                    <Accordion.Item
+                        value={post.summary}
+                        className="mt-0 rounded-b-lg"
+                    >
                         <Accordion.Control className="text-sm rounded-b-lg sm:text-base transition-all font-overpass pt-[17px] dark:bg-darkblue bg-[#dfdbdb] dark:text-white">
                             {post.summary}
                         </Accordion.Control>
